9a8eea86961d3348ef5b29b1ebc6e432c0190b2d
Hugo de Moraes <humoraes@rd.com.br>
2023-07-18 23:16:11 -0300

    :sparkles: list exercises history

---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,32 @@
+import { HStack, Heading, Text, VStack } from "native-base";
+
+type Props = {
+  title: string;
+  description: string;
+}
+
+export function HistoryCard({title, description}: Props) {
+  return (
+    <HStack
+      w="full"
+      px={5}
+      py={4}
+      mb={3}
+      bg="gray.600"
+      rounded="md"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <VStack mr={5}>
+        <Heading color="white" fontSize="md" textTransform="capitalize">
+          {title}
+        </Heading>
+        <Text color="gray.100" fontSize="lg" numberOfLines={1}>
+          {description}
+        </Text>
+      </VStack>
+
+      <Text color="gray.300" fontSize="md">22:45</Text>
+    </HStack>
+  );
+}
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,50 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { Heading, VStack, SectionList, Text } from "native-base";
+
+import { HistoryCard } from "@components/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader";
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "18.07.23",
+      data: [
+        { title: "Costas", description: "Puxada frontal" },
+        { title: "Costas", description: "Remada unilateral" },
+      ],
+    },
+    {
+      title: "17.07.23",
+      data: [{ title: "Costas", description: "Puxada frontal" }],
+    },
+  ]);
+
   return (
-    <Center flex={1}>
-      <Text>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item.description}
+        renderItem={({ item }) => (
+          <HistoryCard title={item.title} description={item.description} />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        contentContainerStyle={
+          !exercises.length && { flex: 1, justifyContent: "center" }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrados ainda.
+          </Text>
+        )}
+        px={8}
+        showsVerticalScrollIndicator={false}
+      />
+    </VStack>
   );
 }
